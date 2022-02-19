d67142d87343c7564f4f6b1bca21e16beb61a79f
dctullis <singulariitek@gmail.com>
2022-02-18 18:29:35 -0600

    components

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './menu-item.styles.scss'
+
+const MenuItem = ({ title, id, image }) => {
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${image})`
+            }}
+            className="menu-item">
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+import Directory from '../../components/directory/directory'
+
+/*
+comment
+
+*/
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+        <Directory />
+    </div>
+  )
+}
+
+export default HomePage
