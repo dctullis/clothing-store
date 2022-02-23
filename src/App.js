78255e88a2ac3f7f18ec1c5497afb9ee02a64e81
dctullis <singulariitek@gmail.com>
2022-02-22 19:26:40 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<HomePage />}/>
-      <Route path="/shop" element={<ShopPage/>}/>
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </div>
   );
 }
 
